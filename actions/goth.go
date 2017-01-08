c1b895efff0a6f6ddfa3479b56126d29a78cbc0a
Ken Pepple <ken@pepple.io>
2017-01-08 12:07:50 -0800

    add goth auth generator for twitter and facebook

---
+++ actions/goth.go
@@ -0,0 +1,34 @@
+package actions
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/gobuffalo/buffalo"
+	"github.com/markbates/goth"
+	"github.com/markbates/goth/gothic"
+	"github.com/markbates/goth/providers/facebook"
+	"github.com/markbates/goth/providers/twitter"
+)
+
+func init() {
+	gothic.Store = App().SessionStore
+
+	goth.UseProviders(
+		twitter.New(os.Getenv("TWITTER_KEY"), os.Getenv("TWITTER_SECRET"), fmt.Sprintf("%s%s", App().Host, "/auth/twitter/callback")),
+		facebook.New(os.Getenv("FACEBOOK_KEY"), os.Getenv("FACEBOOK_SECRET"), fmt.Sprintf("%s%s", App().Host, "/auth/facebook/callback")),
+	)
+
+	app := App().Group("/auth")
+	app.GET("/{provider}", buffalo.WrapHandlerFunc(gothic.BeginAuthHandler))
+	app.GET("/{provider}/callback", AuthCallback)
+}
+
+func AuthCallback(c buffalo.Context) error {
+	user, err := gothic.CompleteUserAuth(c.Response(), c.Request())
+	if err != nil {
+		return c.Error(401, err)
+	}
+	// Do something with the user, maybe register them/sign them in
+	return c.Render(200, r.JSON(user))
+}
