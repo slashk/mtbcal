fc134242b7e4524ebcee95bb891d2135be59199c
Ken Pepple <ken@pepple.io>
2017-01-09 13:50:46 -0800

    add templates and update code from buffalo examples

--- actions/users.go
+++ actions/users.go
@@ -16,6 +16,19 @@ func init() {
 	App().Resource("/users", resource)
 }
 
+func findUserFromParam(c buffalo.Context) (models.User, error) {
+	var u models.User
+	k, err := c.ParamInt("user_id")
+	if err != nil {
+		// if no ID (int) found, lookup by login name (unique)
+		k := c.Param("user_id")
+		err = models.DB.Scope(models.ByLogin(k)).First(&u)
+	} else {
+		err = models.DB.Find(&u, k)
+	}
+	return u, err
+}
+
 // List finds all registered users
 func (v *UsersResource) List(c buffalo.Context) error {
 	// limit this to admin
@@ -31,15 +44,7 @@ func (v *UsersResource) List(c buffalo.Context) error {
 
 // Show default implementation.
 func (v *UsersResource) Show(c buffalo.Context) error {
-	var u models.User
-	k, err := c.ParamInt("user_id")
-	if err != nil {
-		// if no ID (int) found, lookup by login name (unique)
-		k := c.Param("user_id")
-		err = models.DB.Scope(models.ByLogin(k)).First(&u)
-	} else {
-		err = models.DB.Find(&u, k)
-	}
+	u, err := findUserFromParam(c)
 	if err != nil {
 		return c.Render(500, r.String("User id not found"))
 	}
@@ -49,7 +54,8 @@ func (v *UsersResource) Show(c buffalo.Context) error {
 
 // New creates an empty user for creation
 func (v *UsersResource) New(c buffalo.Context) error {
-	return c.Render(200, r.String("Users#New"))
+	c.Set("user", models.User{})
+	return c.Render(200, r.HTML("users/new.html"))
 }
 
 // Create default implementation.
@@ -59,45 +65,56 @@ func (v *UsersResource) Create(c buffalo.Context) error {
 	if err != nil {
 		return c.Render(500, r.String("bad user data rejected"))
 	}
-	err = models.DB.Save(&u)
+	err = models.DB.Create(&u)
 	if err != nil {
 		return c.Render(500, r.String("user cannot be saved to DB"))
 	}
-	return c.Render(200, r.String(u.String()))
+	return c.Redirect(301, "/users/%d", &u.ID)
 }
 
 // Edit default implementation.
 func (v *UsersResource) Edit(c buffalo.Context) error {
 	// TODO admin middleware
-	var u models.User
-	k, err := c.ParamInt("user_id")
-	if err != nil {
-		// if no ID (int) found, lookup by login name (unique)
-		k := c.Param("user_id")
-		err = models.DB.Scope(models.ByLogin(k)).First(&u)
-	} else {
-		err = models.DB.Find(&u, k)
-	}
+	u, err := findUserFromParam(c)
 	if err != nil {
 		return c.Render(500, r.String("User id not found"))
 	}
+	c.Set("user", u)
 	// TODO show form passing u into it
-	return c.Render(200, r.String("Users#Edit"))
+	return c.Render(200, r.HTML("users/edit.html"))
+	// return c.Render(200, r.String("Users#Edit"))
 }
 
 // Update default implementation.
 func (v *UsersResource) Update(c buffalo.Context) error {
-	return c.Render(200, r.String("Users#Update"))
+	u, err := findUserFromParam(c)
+	if err != nil {
+		return c.Render(404, r.String("user cannot be found"))
+	}
+	err = c.Bind(&u)
+	if err != nil {
+		return c.Render(500, r.String("bad user data rejected"))
+	}
+	err = models.DB.Update(&u)
+	if err != nil {
+		// return errors.WithStack(err)
+	}
+	err = models.DB.Reload(&u)
+	if err != nil {
+		// return errors.WithStack(err)
+	}
+	// return c.Render(200, r.String("Users#Update"))
+	return c.Redirect(301, "/users/%d", u.ID)
 }
 
 // Destroy user
 func (v *UsersResource) Destroy(c buffalo.Context) error {
 	// TODO admin middleware
-	k, err := c.ParamInt("user_id")
+	u, err := findUserFromParam(c)
 	if err != nil {
 		return c.Render(404, r.String("user cannot be found"))
 	}
-	err = models.DB.Destroy(&models.User{ID: k})
+	err = models.DB.Destroy(&models.User{ID: u.ID})
 	if err != nil {
 		return c.Render(500, r.String("user cannot be deleted from DB"))
 	}
