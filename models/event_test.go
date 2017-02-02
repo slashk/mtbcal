f5e2040cc8e9a1966cc1ac265196b654c50489c7
Ken Pepple <ken@pepple.io>
2017-02-02 15:57:21 -0800

    first event model test

--- models/event_test.go
+++ models/event_test.go
@@ -1,7 +1,32 @@
 package models_test
 
-import "testing"
+import (
+	"testing"
+	"time"
 
-func Test_Event(t *testing.T) {
-	t.Fatal("This test needs to be implemented!")
+	"github.com/slashk/mtbcal/models"
+	"github.com/stretchr/testify/require"
+)
+
+func TestEventString(t *testing.T) {
+	r := require.New(t)
+	e := models.Event{
+		Name:         "BigBoy",
+		Location:     "Folsom, CA",
+		State:        "CA",
+		WebReg:       true,
+		Active:       true,
+		PublishedAt:  time.Now(),
+		StartDate:    time.Now(),
+		EndDate:      time.Now(),
+		RegOpenDate:  time.Now(),
+		RegCloseDate: time.Now(),
+		Description:  "This is the bigboy event",
+		URL:          "http://www.mtbcalendar.com",
+		UserID:       "admin",
+		Country:      "US",
+		Lng:          -95.2353,
+		Lat:          38.9717,
+	}
+	r.Contains(e.String(), "BigBoy")
 }
