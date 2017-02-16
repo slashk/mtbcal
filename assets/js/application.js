c1b2044cf4cfecb4c2ed877ef7765fed94017ce4
Ken Pepple <ken@pepple.io>
2017-02-16 14:30:41 -0800

    port javascript to make EndDate always fall after StartDate

--- assets/js/application.js
+++ assets/js/application.js
@@ -2,5 +2,28 @@ require('expose?$!expose?jQuery!jquery');
 require("bootstrap/dist/js/bootstrap.js");
 
 $(() => {
+    $('[data-toggle="tooltip"]').tooltip();
+    if ($('#StartDate').length) {
+      $('#StartDate').blur(function() {
+        start = document.getElementById("StartDate").value;
+        // end = document.getElementById("EndDate").value;
+        startD = document.getElementById("StartDate").valueAsDate;
+        endD = document.getElementById("EndDate").valueAsDate;
+        if (startD > endD) {
+          document.getElementById("EndDate").value = start;
+        }
+      });
+    };
+    if ($('#RegOpenDate').length) {
+      $('#RegOpenDate').blur(function() {
+        start = document.getElementById("RegOpenDate").value;
+        // end = document.getElementById("EndDate").value;
+        startD = document.getElementById("RegOpenDate").valueAsDate;
+        endD = document.getElementById("RegCloseDate").valueAsDate;
+        if (startD > endD) {
+          document.getElementById("RegCloseDate").value = start;
+        }
+      });
+    }
 
 });
