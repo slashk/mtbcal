959fa6a40993474796d697999142d154c3b4b1e3
Ken Pepple <ken@pepple.io>
2017-02-16 19:43:26 -0800

    add validators for date ranges and url plus tests

--- models/event_test.go
+++ models/event_test.go
@@ -30,3 +30,62 @@ func TestEventString(t *testing.T) {
 	}
 	r.Contains(e.String(), "BigBoy")
 }
+
+func TestValidDateRange(t *testing.T) {
+	r := require.New(t)
+
+	eValid := models.Event{
+		StartDate: time.Now().Add(20 * 24 * time.Hour),
+		EndDate:   time.Now().Add(24 * 24 * time.Hour),
+	}
+	r.Equal(eValid.ValidDates(), true, "EndDates after StartDate should be valid")
+
+	eInvalid := models.Event{
+		StartDate: time.Now().Add(20 * 24 * time.Hour),
+		EndDate:   time.Now().Add(4 * 24 * time.Hour),
+	}
+	r.Equal(eInvalid.ValidDates(), false, "EndDate before StartDate should be invalid")
+
+	n := time.Now()
+	eEqual := models.Event{
+		StartDate: n,
+		EndDate:   n,
+	}
+	r.Equal(eEqual.ValidDates(), true, "Equal dates should be valid")
+}
+
+func TestValidWebReg(t *testing.T) {
+	r := require.New(t)
+
+	eValid := models.Event{
+		EndDate:      time.Now().Add(26 * 24 * time.Hour),
+		RegOpenDate:  time.Now().Add(20 * 24 * time.Hour),
+		RegCloseDate: time.Now().Add(24 * 24 * time.Hour),
+	}
+	r.Equal(eValid.ValidWebReg(), true, "RegClose after RegOpen should be valid")
+
+	eInvalid := models.Event{
+		EndDate:      time.Now().Add(26 * 24 * time.Hour),
+		RegOpenDate:  time.Now().Add(20 * 24 * time.Hour),
+		RegCloseDate: time.Now().Add(4 * 24 * time.Hour),
+	}
+	r.Equal(eInvalid.ValidWebReg(), false, "RegClose before RegOpen should be invalid")
+
+	n := time.Now()
+	eEqual := models.Event{
+		EndDate:      time.Now().Add(26 * 24 * time.Hour),
+		RegOpenDate:  n,
+		RegCloseDate: n,
+	}
+	r.Equal(eEqual.ValidWebReg(), true, "Equal Reg should be valid")
+}
+
+func TestValidURL(t *testing.T) {
+	r := require.New(t)
+	valid := models.Event{URL: "http://google.com"}
+	r.True(valid.ValidURL(), "should be valid url")
+	invalid := models.Event{URL: "sdfsdfsf"}
+	r.False(invalid.ValidURL(), "should be invalid url")
+	a := models.Event{URL: "http//google.com"}
+	r.False(a.ValidURL(), "should be invalid url")
+}
