ee89e5941316b7060e4142bf010cb314f13a1f85
Ken Pepple <ken@pepple.io>
2017-03-05 12:33:20 -0800

    upgrade main to buffalo 0.7.4

--- main.go
+++ main.go
@@ -4,14 +4,13 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-	"os"
 
-	"github.com/markbates/going/defaults"
+	"github.com/gobuffalo/envy"
 	"github.com/slashk/mtbcal/actions"
 )
 
 func main() {
-	port := defaults.String(os.Getenv("PORT"), "3000")
+	port := envy.Get("PORT", "3000")
 	log.Printf("Starting mtbcal on %s at port %s\n", actions.App().Host, port)
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), actions.App()))
 }
